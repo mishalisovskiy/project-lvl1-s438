6434e5e92ce28692f5223f048235252e2178ecca
Mykhailo <mishalisovskiy@gmail.com>
2019-02-05 18:29:32 +0200

    Working files adjusted according to ESlint

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import readlineSync from 'readline-sync';
 
 const askUserName = () => {
-	const userName = readlineSync.question('May I have your name? ');
+  const userName = readlineSync.question('May I have your name? ');
 
-	console.log(`Hello ${userName}!`);
-}
+  console.log(`Hello ${userName}!`);
+};
 
 export default askUserName;
