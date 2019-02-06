2e14a147aa87d88dee8b06251587d7d3376e350a
Mykhailo <mishalisovskiy@gmail.com>
2019-02-06 23:41:58 +0200

    All games may be started from brain-games.js

--- src/bin/brain-games.js
+++ src/bin/brain-games.js
@@ -1,4 +1,22 @@
 #!/usr/bin/env node
-import { playBrainGames } from '../games/games';
+import playBrainEven from '../games/even';
+import playBrainCalc from '../games/calc';
+import readlineSync from 'readline-sync';
+import { userName } from '..';
 
-playBrainGames();
+const chooseTheGame = () => {
+  console.log(`Please choose the game, ${userName}`);
+  const gameChosen = readlineSync.question('1: Brain-Even, 2: Brain-Calc ');
+  switch (gameChosen) {
+    case '1':
+      playBrainEven();
+      break;
+    case '2':
+      playBrainCalc();
+      break;
+    default:
+      console.log('Invalid key. Please try again.');
+  }
+};
+
+chooseTheGame();
