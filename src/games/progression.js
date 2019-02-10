a41d86fbb216d0cc9b8428371dc56d2dd9c5c38d
Mykhailo <mishalisovskiy@gmail.com>
2019-02-10 02:31:46 +0200

    Small changes to progression game

--- src/games/progression.js
+++ src/games/progression.js
@@ -3,19 +3,20 @@ import getNumber from '../utils';
 import * as _ from 'lodash';
 
 const gameTask = 'What number is missing in the progression?';
-const numToAdd = getNumber(0, 20);
-const startingPoint = getNumber(0, 100);
-
-const progressiveArray = () => {
-  const result = [];
-
-  for (let i = startingPoint; result.length <= 10; i += numToAdd) {
-    result.push(i);
-  }
-  return result;
-};
 
 const playProgression = () => {
+  const numToAdd = getNumber(0, 20);
+  const startingPoint = getNumber(0, 100);
+
+  const progressiveArray = () => {
+    const result = [];
+
+    for (let i = startingPoint; result.length <= 10; i += numToAdd) {
+      result.push(i);
+    }
+    return result;
+  };
+
   const arrayToEvaluate = progressiveArray();
   const replacedItemInArray = _.sample(arrayToEvaluate);
   const arrayToString = arrayToEvaluate.reduce((acc, item) => (`${acc} ${item}`), '');
