b3c40175ca975d63ff09635a6de40a8645ff12fa
Mykhailo <mishalisovskiy@gmail.com>
2019-02-10 18:56:39 +0200

    Filan Release!!!

--- src/index.js
+++ src/index.js
@@ -2,10 +2,6 @@ import readlineSync from 'readline-sync';
 
 const roundsLimit = 3;
 
-const getWinMessage = () => {
-  console.log('Congratulations! You won.');
-};
-
 const buildGame = (game, task) => {
   const userName = readlineSync.question('May I have your name? ');
   console.log(`Hello ${userName}!`);
@@ -13,20 +9,18 @@ const buildGame = (game, task) => {
 
   const iterateGame = (counter) => {
     if (counter === roundsLimit) {
-      return getWinMessage();
-    } {
+      console.log('Congratulations! You won.');
+    } else {
       const { question, answer } = game();
       console.log(question);
       const userAnswer = readlineSync.question('Your answer: ');
 
       if (userAnswer === answer) {
         console.log('Correct!');
-        return iterateGame(counter + 1);
-      }
-      const getFailMessage = () => {
+        iterateGame(counter + 1);
+      } else {
         console.log(`'${userAnswer}' is the wrong answer ;(. The correct answer was '${answer}'. \nLet's try again, ${userName}!`);
-      };
-      return getFailMessage();
+      }
     }
   };
   iterateGame(0);
