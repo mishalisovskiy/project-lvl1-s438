788a50ddc1e033df34dc1f5ed16ace666afb9289
Mykhailo <mishalisovskiy@gmail.com>
2019-02-12 23:04:06 +0200

    FINAL RELEASE!

--- src/index.js
+++ src/index.js
@@ -10,18 +10,18 @@ const buildGame = (game, task) => {
   const iterateGame = (counter) => {
     if (counter === roundsLimit) {
       console.log('Congratulations! You won.');
-    } else {
-      const { question, answer } = game();
-      console.log(question);
-      const userAnswer = readlineSync.question('Your answer: ');
+      return;
+    } 
+    const { question, answer } = game();
+    console.log(question);
+    const userAnswer = readlineSync.question('Your answer: ');
 
-      if (userAnswer === answer) {
-        console.log('Correct!');
-        iterateGame(counter + 1);
-      } else {
-        console.log(`'${userAnswer}' is the wrong answer ;(. The correct answer was '${answer}'. \nLet's try again, ${userName}!`);
-      }
-    }
+    if (userAnswer === answer) {
+      console.log('Correct!');
+      iterateGame(counter + 1);
+    } 
+      console.log(`'${userAnswer}' is the wrong answer ;(. The correct answer was '${answer}'. \nLet's try again, ${userName}!`);
+    
   };
   iterateGame(0);
 };
