a398939f1295952200cd40d1c02cbb2208e2b96e
Mykhailo <mishalisovskiy@gmail.com>
2019-02-12 23:05:54 +0200

    Line break added

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ const buildGame = (game, task) => {
     if (counter === roundsLimit) {
       console.log('Congratulations! You won.');
       return;
-    } 
+    }
     const { question, answer } = game();
     console.log(question);
     const userAnswer = readlineSync.question('Your answer: ');
@@ -19,9 +19,9 @@ const buildGame = (game, task) => {
     if (userAnswer === answer) {
       console.log('Correct!');
       iterateGame(counter + 1);
-    } 
-      console.log(`'${userAnswer}' is the wrong answer ;(. The correct answer was '${answer}'. \nLet's try again, ${userName}!`);
-    
+    }
+    console.log(`'${userAnswer}' is the wrong answer ;(. The correct answer was '${answer}'.`);
+    console.log(`Let's try again, ${userName}!`);
   };
   iterateGame(0);
 };
