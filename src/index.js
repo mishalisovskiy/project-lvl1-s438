90cdc0fd4ea055883509c5ac307e07136a349920
Mykhailo <mishalisovskiy@gmail.com>
2019-02-19 02:22:45 +0200

    Modification to include variable number of rounds

--- src/index.js
+++ src/index.js
@@ -1,16 +1,14 @@
 import readlineSync from 'readline-sync';
 
-const roundsLimit = 3;
-
 const buildGame = (game, task) => {
   const userName = readlineSync.question('May I have your name? ');
   console.log(`Hello ${userName}!`);
   console.log(task);
+  const roundsLimit = readlineSync.question('How many rounds would you like to play? ');
 
   const iterateGame = (counter) => {
-    if (counter === roundsLimit) {
-      console.log('Congratulations! You won.');
-      return;
+    if (counter === Number(roundsLimit)) {
+      return console.log('Congratulations! You won.');
     }
     const { question, answer } = game();
     console.log(question);
@@ -19,9 +17,10 @@ const buildGame = (game, task) => {
     if (userAnswer === answer) {
       console.log('Correct!');
       iterateGame(counter + 1);
+    } else {
+      console.log(`'${userAnswer}' is the wrong answer ;(. The correct answer was '${answer}'`);
+      console.log(`Let's try again, ${userName}!`)    
     }
-    console.log(`'${userAnswer}' is the wrong answer ;(. The correct answer was '${answer}'.`);
-    console.log(`Let's try again, ${userName}!`);
   };
   iterateGame(0);
 };
